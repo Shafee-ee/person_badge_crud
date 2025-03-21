81ed5dc72c2841fe72ed0349d1d37742cea7e9f4
shafee_azeem <shafeeghani@gmail.com>
2025-03-22 03:01:12 +0530

    badge fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect, useState } from 'react'
+import Badge from './components/Badge';
+import { fetchPeople } from "./api";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //state to store the List of people
+  const [people, setPeople] = useState([]);
+  useEffect(() => {
+    const getPeople = async () => {
+      const data = await fetchPeople();
+      if (data) {
+        setPeople(data);
+      }
+    };
+    getPeople();
+
+  }, [])
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h1>People Badges</h1>
+      <div className="badge-list">
+        {people.map((People) => {
+          <Badge key={person._id} name={person.name} age={person.age} image={person.image} />
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
--- client/src/components/Badge.jsx
+++ client/src/components/Badge.jsx
@@ -1,3 +1,19 @@
 //Import React to use JSX
 
 import React from "react";
+import "./styles/Badge.css";
+
+//Define the badge component receive name, age and image as props 
+
+const Badge = ({ name, age, image }) => {
+    return (
+        <div className="badge-container">
+            <img src={`http://localhost:5000/${image}`} alt="{name}" className="Bade-image" />
+
+            <h2 className="badge-name">{name}</h2>
+            <p className="badge-age">Age:{age}</p>
+        </div>
+    );
+};
+
+export default Badge;
