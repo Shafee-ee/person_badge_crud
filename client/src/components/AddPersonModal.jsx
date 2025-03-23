9c10263012a8e4c8b666b34d64d2f9f1d87983ba
shafee_azeem <shafeeghani@gmail.com>
2025-03-24 03:09:52 +0530

    loading and complete message

--- client/src/components/AddPersonModal.jsx
+++ client/src/components/AddPersonModal.jsx
@@ -8,11 +8,17 @@ const AddPersonModal = ({ isOpen, onClose }) => {
     const [name, setName] = useState("");
     const [age, setAge] = useState("");
     const [image, setImage] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [completed, setCompleted] = useState(false);
 
     //handle form submission
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        //loading
+        setLoading(true);
+        setCompleted(false);
 
         //create form data 
         const formData = new FormData();
@@ -23,40 +29,61 @@ const AddPersonModal = ({ isOpen, onClose }) => {
             formData.append("image", image);
         }
 
+        console.log("FormData contents:", [...formData.entries()]);
         //send data fot backend
-        const newPerson = await createPerson(formData);
-        if (newPerson) {
-            onPersonAdded(newPerson);
-            onClose();
-        }
+
+
+
+        setTimeout(async () => {
+            const newPerson = await createPerson(formData);
+            if (newPerson) {
+                onPersonAdded(newPerson);
+
+                setLoading(false);
+                setCompleted(true);
+
+                setTimeout(() => {
+                    setCompleted(false);
+                    onClose();
+                }, 2000);
+            }
+        }, 1000);
+
+
     }
 
     return (
         <div className="modal-overlay">
             <div className="modal-content">
                 <h2>Add a New Person</h2>
-                <form onSubmit={handleSubmit}>
-                    <label>Name:</label>
-                    <input type="text"
-                        name="name"
-                        onChange={(e) => setName(e.target.value)}
-                        required />
+                {loading ? (
+                    <p className="loading-message">Adding Person...</p>
+                ) : completed ? (
+                    <p className="success-message">Success!</p>
+                ) : (
+                    <form onSubmit={handleSubmit}>
+                        <label>Name:</label>
+                        <input type="text"
+                            name="name"
+                            onChange={(e) => setName(e.target.value)}
+                            required />
 
-                    <label>Age:</label>
-                    <input type="number"
-                        name="Age"
-                        onChange={(e) => setAge(e.target.value)}
-                        required />
+                        <label>Age:</label>
+                        <input type="number"
+                            name="Age"
+                            onChange={(e) => setAge(e.target.value)}
+                            required />
 
-                    <label>Upload Image:</label>
-                    <input type="file"
-                        name="image"
-                        accept="image/*"
-                        onChange={(e) => { e.target.value }}
-                    />
+                        <label>Upload Image:</label>
+                        <input type="file"
+                            name="image"
+                            accept="image/*"
+                            onChange={(e) => setImage(e.target.files[0])}
+                        />
 
-                    <button type="submit" className="add-button">Add person</button>
-                </form>
+                        <button type="submit" className="add-button">Add person</button>
+                    </form>
+                )}
                 <button className="close-button" onClick={onClose}>X</button>
             </div>
         </div>
